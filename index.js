a9aaf8df410d043ae32af9c6cbd0c94d982e6da4
aaron <xakong@outlook.com>
2019-03-10 06:53:31 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Main starting point of app.
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');         // http request logger 
+const app = express();
+const router = require('./router');
+// nodemon.js, auto restart when code changes
+
+// App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app); // create http (native node library) server for working low lvl http requests incoming. Anything coming in, forward on to the app (express)
+server.listen(port);
+console.log('Server is lisTening on:', port);
